627491562f5805a8f6f28723ebdd5da536d8d939
rafaelbreno <rafael_breno@outlook.com>
2022-11-15 17:01:58 -0300

    feat: Open blue window

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,10 @@ fn main() {
     let display = glium::Display::new(wb, cb, &event_loop).unwrap();
 
     event_loop.run(move |ev, _, control_flow| {
+        let mut target = display.draw();
+        target.clear_color(0.0, 0.0, 1.0, 1.0);
+        target.finish().unwrap();
+
         let next_frame_time = std::time::Instant::now() + 
             std::time::Duration::from_nanos(16_666_667);
         *control_flow = glutin::event_loop::ControlFlow::WaitUntil(next_frame_time);
